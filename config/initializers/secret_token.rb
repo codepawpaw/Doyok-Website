8925ca64f821dbc9f8629f5b869283c2b77e7b67
Jonathan <jonathannael12@gmail.com>
2017-04-17 17:41:45 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoyokWebsite::Application.config.secret_token = '11cecba5e6cf99c0f24414e168f8d7b3899e13cfb86709f665e983205990b4bbd5a2abed8df711fecb5476d7479e28c727247a3178963f2d077ec4b1ffc46b01'
